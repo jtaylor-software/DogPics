4ce4f26260656f8fd1a4024baf7dd2561cc5e8c9
Jeremy Taylor <jtaylor@jctaylor.org>
2023-01-15 12:02:25 -0600

    Initial Commit

---
+++ DogPics/DogPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogPicsApp.swift
+//  DogPics
+//
+//  Created by Jeremy Taylor on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DogPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
